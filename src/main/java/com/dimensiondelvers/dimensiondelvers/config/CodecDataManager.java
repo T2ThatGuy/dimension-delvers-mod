f7596c274e84bebf362944181a3071ec321face0
Charlie <66382380+T2ThatGuy@users.noreply.github.com>
2025-02-10 20:29:14 +0000

    chore: update CodecDataManager to support 1.21.4

--- src/main/java/com/dimensiondelvers/dimensiondelvers/config/CodecDataManager.java
+++ src/main/java/com/dimensiondelvers/dimensiondelvers/config/CodecDataManager.java
@@ -6,10 +6,12 @@ import com.mojang.logging.LogUtils;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.JsonOps;
+import net.minecraft.resources.FileToIdConverter;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
 import java.util.*;
@@ -24,37 +26,13 @@ import java.util.*;
  * Intended that this class is extended upon to provide additional methods
  * on top of this class for per config functionality.
  */
-public class CodecDataManager<TCodec> extends SimpleJsonResourceReloadListener {
+public class CodecDataManager<TCodec> extends SimpleJsonResourceReloadListener<TCodec> {
 
     private final static Gson GSON = new Gson();
-    private final Codec<TCodec> codec;
-
-    private final String directory;
     private Map<ResourceLocation, TCodec> data = new HashMap<>();
 
     public CodecDataManager(Codec<TCodec> codec, String directory) {
-        super(GSON, directory);
-        this.codec = codec;
-        this.directory = directory;
-    }
-
-    @Override
-    protected void apply(Map<ResourceLocation, JsonElement> object, ResourceManager resourceManager, ProfilerFiller profiler) {
-
-        data = new HashMap<>();
-
-        for (Map.Entry<ResourceLocation, JsonElement> entry : object.entrySet()) {
-            ResourceLocation key = entry.getKey();
-            JsonElement element = entry.getValue();
-
-            DataResult<TCodec> parsedConfig = codec.parse(JsonOps.INSTANCE, element);
-            if (parsedConfig.isSuccess()) {
-                data.put(key, parsedConfig.getOrThrow());
-                continue;
-            }
-
-            LogUtils.getLogger().error("Error in config ({}) {}", directory, parsedConfig.error().toString());
-        }
+        super(codec, FileToIdConverter.json(directory));
     }
 
     public Map<ResourceLocation, TCodec> getData() {
@@ -64,5 +42,15 @@ public class CodecDataManager<TCodec> extends SimpleJsonResourceReloadListener {
     @Nullable
     public TCodec getDataByResource(ResourceLocation location) {
         return data.get(location);
+    }
+
+    @Override
+    protected void apply(
+            @NotNull Map<ResourceLocation, TCodec> object,
+            @NotNull ResourceManager resourceManager,
+            @NotNull ProfilerFiller profiler
+    ) {
+        data = new HashMap<>();
+        data.putAll(object);
     }
 }
--- src/main/java/com/dimensiondelvers/dimensiondelvers/config/ConfigManager.java
+++ src/main/java/com/dimensiondelvers/dimensiondelvers/config/ConfigManager.java
@@ -3,9 +3,10 @@ package com.dimensiondelvers.dimensiondelvers.config;
 import com.dimensiondelvers.dimensiondelvers.DimensionDelvers;
 import com.dimensiondelvers.dimensiondelvers.config.configs.ExampleConfig;
 import com.dimensiondelvers.dimensiondelvers.config.configs.RunegemTierConfig;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.event.AddReloadListenerEvent;
+import net.neoforged.neoforge.event.AddServerReloadListenersEvent;
 
 
 @EventBusSubscriber(modid = DimensionDelvers.MODID, bus = EventBusSubscriber.Bus.GAME)
@@ -15,8 +16,8 @@ public class ConfigManager {
     public static final RunegemTierConfig RUNEGEM_TIER = new RunegemTierConfig();
 
     @SubscribeEvent
-    public static void onAddReloadListeners(AddReloadListenerEvent event) {
-        event.addListener(EXAMPLE);
-        event.addListener(RUNEGEM_TIER);
+    public static void onAddReloadListeners(AddServerReloadListenersEvent event) {
+        event.addListener(ResourceLocation.fromNamespaceAndPath(DimensionDelvers.MODID, "example_config"), EXAMPLE);
+        event.addListener(ResourceLocation.fromNamespaceAndPath(DimensionDelvers.MODID, "runegem_tiers"), RUNEGEM_TIER);
     }
 }
